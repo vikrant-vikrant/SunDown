5086895d81c1891016742f953e968b83bbed9ada
Vikrant <vikrant2003vikrant@gmail.com>
2025-07-05 12:02:53 +0530

    complete design,project & execution in page4

--- app.js
+++ app.js
@@ -15,3 +15,40 @@ elems.forEach(function (ele) {
     fixed.style.display = "none";
   });
 });
+function removeSt() {
+  let tab = document.querySelectorAll(".head h1");
+  tab.forEach(function (tb) {
+    tb.removeAttribute("class", "style");
+  });
+}
+let rightImg = document.querySelector(".right-img");
+let design = document.querySelector(".design");
+let project = document.querySelector(".project");
+let executable = document.querySelector(".executable");
+let para = document.querySelector(".head p");
+design.addEventListener("click", function () {
+  removeSt();
+  design.setAttribute("class", "style");
+  let img = design.getAttribute("data-img");
+  rightImg.style.backgroundImage = `url(${img})`;
+  para.innerHTML =
+    "Our team works with our clients to refine an idea and concept into an executable design. We create a final design that encompasses the brand narrative to bring stories to life and provide end-to-end design solutions from concept, design, and architectural drawings to 3D renderings.";
+});
+
+project.addEventListener("click", function () {
+  removeSt();
+  project.setAttribute("class", "style");
+  let img = project.getAttribute("data-img");
+  rightImg.style.backgroundImage = `url(${img})`;
+  para.innerHTML =
+    "Once we have a design, our production team takes the lead in bringing it to life. We manage all stages of the project, from build specifications and technical drawings to site surveys, vendor management, and 2D & 3D production. We have an extensive network of partners to meet each unique design and project need.";
+});
+
+executable.addEventListener("click", function () {
+  removeSt();
+  executable.setAttribute("class", "style");
+  let img = executable.getAttribute("data-img");
+  rightImg.style.backgroundImage = `url(${img})`;
+  para.innerHTML =
+    " We’re with you every step of the way, from the project initiation to launch day. Our production and design teams are onsite to direct and guide the process down to the last point of completion, ensuring success across the built space and experience.";
+});
